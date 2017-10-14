37a59a7148f64ab4347d3eede65e8e95a3b14537
Andrew Horn <arhorn@smcm.edu>
2017-10-14 17:25:23 +0700

    Added projects page

--- server.js
+++ server.js
@@ -63,6 +63,12 @@ app.get('/bad', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
+})
+
 app.listen(port, () => {
   console.log(`Server is up at port ${port}`)
 });
